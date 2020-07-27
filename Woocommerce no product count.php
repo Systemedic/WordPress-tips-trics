6359486dc339a636207353637f1b2ca4a00460a2
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-27 10:14:04 +0200

    Update Woocommerce no product count.php

--- Woocommerce no product count.php
+++ Woocommerce no product count.php
@@ -1,5 +1,4 @@
-// Hide Product Category Count
-add_filter( 'woocommerce_subcategory_count_html', 'prima_hide_subcategory_count' );
-function prima_hide_subcategory_count() {
-  /* empty - no count */
-}
+/**
+ * Hide category product count in product archives
+ */
+add_filter( 'woocommerce_subcategory_count_html', '__return_false' );
