158be717bd4a6719428f811e8e8f831041ec4f5b
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-27 10:11:06 +0200

    Create Woocommerce no product count.php
    With this snippet you can hide the product count behind a category on the shop pages.

---
+++ Woocommerce no product count.php
@@ -0,0 +1,5 @@
+// Hide Product Category Count
+add_filter( 'woocommerce_subcategory_count_html', 'prima_hide_subcategory_count' );
+function prima_hide_subcategory_count() {
+  /* empty - no count */
+}
