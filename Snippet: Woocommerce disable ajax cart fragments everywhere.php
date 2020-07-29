d3bb6995cf4c37d9183127c2d24ef82d27edde5a
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:45:15 +0200

    Update Snippet: Woocommerce disable ajax cart fragments everywhere.php

--- Snippet: Woocommerce disable ajax cart fragments everywhere.php
+++ Snippet: Woocommerce disable ajax cart fragments everywhere.php
@@ -1,12 +1,11 @@
 /**
- * @snippet       Disable WooCommerce Ajax Cart Fragments Everywhere
+ * @snippet       Disable WooCommerce Ajax Cart Fragments Everywhere. 
  * @author        Systemedic
- * @compatible    WooCommerce 3.6.4 and up
- * @URL		        https://systemedic.nl/
+ * @URL		         https://systemedic.nl/
  */
  
-add_action( 'wp_enqueue_scripts', 'bbloomer_disable_woocommerce_cart_fragments', 11 ); 
+add_action( 'wp_enqueue_scripts', 'systemedic_disable_woocommerce_cart_fragments', 11 ); 
  
-function bbloomer_disable_woocommerce_cart_fragments() { 
+function systemedic_disable_woocommerce_cart_fragments() { 
    wp_dequeue_script( 'wc-cart-fragments' ); 
 }
