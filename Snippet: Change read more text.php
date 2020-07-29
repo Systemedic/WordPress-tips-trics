04b17ef0cd5b63073c787d451b00c596983c3556
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:41:08 +0200

    Update Snippet: Change read more text.php

--- Snippet: Change read more text.php
+++ Snippet: Change read more text.php
@@ -1,5 +1,9 @@
-<?php
-// Changing excerpt read more text
+/**
+* @snippet       Change excerpt read more text
+* @author        Systemedic
+* @URL			        https://systemedic.nl/
+*/
+
 function smartwp_change_excerpt_more_text( $more ){
  global $post;
  return '&hellip; <a class="read-more" href="'.get_permalink($post->ID).'" title="'.esc_attr(get_the_title($post->ID)).'">'.'Read More &raquo;'.'</a>';
