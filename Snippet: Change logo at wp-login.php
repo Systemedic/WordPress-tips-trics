8e697e9ba8294194c34f9d9367398d4526195d0f
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:42:12 +0200

    Update Snippet: Change logo at wp-login.php

--- Snippet: Change logo at wp-login.php
+++ Snippet: Change logo at wp-login.php
@@ -1,5 +1,9 @@
-<php
-//Change logo at wp-login.php page. Ideal size for logo: 274 x 63 pixels
+/**
+* @snippet       Change logo at wp-login.php page. Ideal size for logo: 274 x 63 pixels
+* @author        Systemedic
+* @URL			     https://systemedic.nl/
+*/
+
 function custom_admin_logo() {
 echo '<style type="text/css">
 #header-logo { background-image: url('.get_bloginfo('template_directory').'/images/admin_logo.png) !important; }
