1b204df6d354319f24659783bb9c7f07e140cdd8
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:41:33 +0200

    Update Snippet: WooCommerce redirect to shop after login.php

--- Snippet: WooCommerce redirect to shop after login.php
+++ Snippet: WooCommerce redirect to shop after login.php
@@ -1,3 +1,9 @@
+/**
+* @snippet       Custom Redirect after logging in to my account for Woocommerce
+* @author        Systemedic
+* @URL			     https://systemedic.nl/
+*/
+
 add_filter('woocommerce_login_redirect', 'pro_login_redirect');
 function pro_login_redirect( $redirect_to ) {
 $redirect_to = get_permalink( 700 ); //Fill in the page ID between the ()
