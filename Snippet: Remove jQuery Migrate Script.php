8d7428fb51b0f7db0ad8aaebdd82bd398671ebab
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 17:17:43 +0200

    Create Snippet: Remove jQuery Migrate Script.php

---
+++ Snippet: Remove jQuery Migrate Script.php
@@ -0,0 +1,16 @@
+/**
+* @snippet        Remove jQuery Migrate Script.php
+* @author         Systemedic
+* @URL            https://systemedic.nl/
+*/
+
+if ( ! function_exists( 'evolution_remove_jquery_migrate' ) ) :
+
+function evolution_remove_jquery_migrate( &$scripts) {
+    if(!is_admin()) {
+        $scripts->remove( 'jquery');
+        $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
+    }
+}
+add_filter( 'wp_default_scripts', 'evolution_remove_jquery_migrate' );
+endif;
