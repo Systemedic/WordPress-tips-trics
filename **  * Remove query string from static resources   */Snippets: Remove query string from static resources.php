43972098de2f1273bf6e2d54c91fd8d3fea9226b
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 17:14:18 +0200

    Create Snippets: Remove query string from static resources.php

---
+++ **  * Remove query string from static resources   */Snippets: Remove query string from static resources.php
@@ -0,0 +1,13 @@
+/**
+* @snippet        Remove query string from static resources
+* @author         Systemedic
+* @URL            https://systemedic.nl/
+*/
+ 
+function remove_cssjs_ver( $src ) {
+    if ( strpos( $src, '?ver=' ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
