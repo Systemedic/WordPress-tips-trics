01b81b178b470df00903f5257c50ecf3733efa55
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:43:35 +0200

    Update Snippet: WooCommerce catalog mode for guest.php

--- Snippet: WooCommerce catalog mode for guest.php
+++ Snippet: WooCommerce catalog mode for guest.php
@@ -4,20 +4,20 @@
 * @URL		 https://systemedic.nl/
 */
  
-add_action( 'init', 'shessvy_hide_price_add_cart_not_logged_in' );
+add_action( 'init', 'systemedic_hide_price_add_cart_not_logged_in' );
  
-function shessvy_hide_price_add_cart_not_logged_in() {
+function systemedic_hide_price_add_cart_not_logged_in() {
     if ( ! is_user_logged_in() ) {
  
         add_filter( 'woocommerce_is_purchasable', '__return_false');
-        add_action( 'woocommerce_single_product_summary', 'shessvy_print_login_to_see', 31 );
-        add_action( 'woocommerce_after_shop_loop_item', 'shessvy_print_login_to_see', 11 );
-        add_action( 'woocommerce_simple_add_to_cart', 'shessvy_print_login_to_see', 30 );
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
- 		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+        add_action( 'woocommerce_single_product_summary', 'systemedic_print_login_to_see', 31 );
+        add_action( 'woocommerce_after_shop_loop_item', 'systemedic_print_login_to_see', 11 );
+        add_action( 'woocommerce_simple_add_to_cart', 'systemedic_print_login_to_see', 30 );
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+ 	remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
     }
 }
  
-function shessvy_print_login_to_see() {
+function systemedic_print_login_to_see() {
     echo '<a href="' . get_permalink(wc_get_page_id('myaccount')) . '" rel="nofollow ugc">' . __('Login or register to see prices', 'theme_name') . '</a>';
 }
