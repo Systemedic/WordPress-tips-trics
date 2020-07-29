317d84697da25446fe726ffa011c37696506abdc
Systemedic <58505361+Systemedic@users.noreply.github.com>
2020-07-29 16:46:33 +0200

    Update Snippet: Woocommerce no product count.php

--- Snippet: Woocommerce no product count.php
+++ Snippet: Woocommerce no product count.php
@@ -1,4 +1,7 @@
 /**
- * Hide category product count in product archives
- */
+* @snippet       Hide category product count in product archives
+* @author        Systemedic
+* @URL			     https://systemedic.nl/
+*/
+
 add_filter( 'woocommerce_subcategory_count_html', '__return_false' );
