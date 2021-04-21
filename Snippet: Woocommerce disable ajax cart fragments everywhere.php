b660b4bbb984f104d73ba8f764a71f8a2273863d
Systemedic <58505361+Systemedic@users.noreply.github.com>
2021-04-21 12:58:43 +0200

    Update Snippet: Woocommerce disable ajax cart fragments everywhere.php

--- Snippet: Woocommerce disable ajax cart fragments everywhere.php
+++ Snippet: Woocommerce disable ajax cart fragments everywhere.php
@@ -1,11 +1,29 @@
-/**
+/****************************************************************
  * @snippet       Disable WooCommerce Ajax Cart Fragments Everywhere. 
  * @author        Systemedic
  * @URL		         https://systemedic.nl/
- */
+*****************************************************************/
  
 add_action( 'wp_enqueue_scripts', 'systemedic_disable_woocommerce_cart_fragments', 11 ); 
  
 function systemedic_disable_woocommerce_cart_fragments() { 
    wp_dequeue_script( 'wc-cart-fragments' ); 
 }
+
+/****************************************************************
+ * @snippet       Disable WooCommerce Ajax Cart Fragments on posts and pages. 
+ * @author        Systemedic
+ * @URL		         https://systemedic.nl/
+*****************************************************************/
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_cart_fragments', 11);
+function dequeue_woocommerce_cart_fragments() {
+if (is_front_page() || is_single() ) wp_dequeue_script('wc-cart-fragments');
+}
+
+/****************************************************************
+ * @snippet       Disable WooCommerce Ajax Cart Fragments on frontpage 
+ * @author        Systemedic
+ * @URL		         https://systemedic.nl/
+*****************************************************************/
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_cart_fragments', 11); 
+function dequeue_woocommerce_cart_fragments() { if (is_front_page()) wp_dequeue_script('wc-cart-fragments'); }
